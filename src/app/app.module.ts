49ef7af56e810f2d48f753e72a8421a17203cf0d
Training <training@Nikolass-MacBook-Pro.local>
2017-07-19 20:23:51 -0400

    Setup initial routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TitleComponent } from './title/title.component';
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+import { ListComponent } from "./list/list.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { 
+    path: 'about', 
+    component: AboutComponent 
+  },
+  { 
+    path: 'contact',      
+    component: ContactComponent 
+  },
+  { 
+    path: 'home',      
+    component: HomeComponent 
+  },
+  { 
+    path: 'list', 
+    component: ListComponent, 
+    data: { title: 'Heroes List' }
+  },
+  { 
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { 
+    path: '**', 
+    component: PageNotFoundComponent 
+  }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponent,
+    ListComponent,
+    AboutComponent,
+    ContactComponent,
+    PageNotFoundComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
