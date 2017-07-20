95da8148eedaf3252d92f23730765c33d3654f25
Training <training@Nikolass-MacBook-Pro.local>
2017-07-19 21:24:52 -0400

    Create button component

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+
+  @Input() text: string = '';
+  @Output() clicked: EventEmitter<null> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
